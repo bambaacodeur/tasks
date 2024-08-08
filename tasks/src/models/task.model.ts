91eb19224fd1ebc02f9a14d2e0aa6968b4a704c7
Khadim SECK <khadim.seck3@orange-sonatel.com>
2024-08-08 15:42:20 +0000

    task api

---
+++ tasks/src/models/task.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Task extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  category: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  status: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  created_at: string;
+
+
+  constructor(data?: Partial<Task>) {
+    super(data);
+  }
+}
+
+export interface TaskRelations {
+  // describe navigational properties here
+}
+
+export type TaskWithRelations = Task & TaskRelations;
